49763e04655bf5bf9d2dd4932d9bee065c3ed558
RafaelVieira12 <rafael11934@hotmail.com>
2023-08-27 08:17:46 +0100

    changing the fies

--- src/app.py
+++ src/app.py
@@ -1,5 +1,8 @@
 import dash
 import dash_bootstrap_components as dbc
+from dash import html, dcc
+from components.sidebar import get_sidebar_layout
+from components.dashboards import get_dashboard_layout
 
 estilos = ["https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css", "https://fonts.googleapis.com/icon?family=Material+Icons", dbc.themes.COSMO]
 dbc_css = "https://cdn.jsdelivr.net/gh/AnnMarieW/dash-bootstrap-templates@V1.0.4/dbc.min.css"
@@ -8,6 +11,33 @@ dbc_css = "https://cdn.jsdelivr.net/gh/AnnMarieW/dash-bootstrap-templates@V1.0.4
 
 app = dash.Dash(__name__, external_stylesheets=estilos + [dbc_css])
 
+
 app.config['suppress_callback_exceptions'] = True
 app.scripts.config.serve_locally = True
 server = app.server
+
+# =========  Layout  =========== #
+content = html.Div(id="page-content")
+
+
+app.layout = dbc.Container(
+    children=[
+        dbc.Row(
+            [
+                dbc.Col(
+                    [
+                        dcc.Location(id='url'),
+                        get_sidebar_layout(app)
+                    ],md=2, style={'background-color': '#966F33','height':'210vh'}),
+                dbc.Col(
+                    [
+                        get_dashboard_layout(app)
+                    ],md=10, style={'background-color': '#f0f0f0', 'height':'210vh'})
+            ]
+        )
+    ],
+    fluid=True,
+)
+
+if __name__ == '__main__':
+    app.run_server(debug=False)
--- src/components/sidebar.py
+++ src/components/sidebar.py
@@ -3,31 +3,32 @@ import dash
 from dash import html, dcc
 from dash.dependencies import Input, Output, State
 import dash_bootstrap_components as dbc
-from app import *
 
 from datetime import datetime, date
 import plotly.express as px
 import numpy as np
 import pandas as pd
 
-
-# ========= Layout ========= #
-layout = dbc.Col([
+def get_sidebar_layout(app):
+    # ========= Layout ========= #
+    layout = dbc.Col([
+        
+        # Sidebar Title
+        html.H1('Food Loss',className='text font-weight-bold font-weight-bolder text-center display-2'),
+        html.Hr(),
+        
+        # Sidebar Image
+        html.Img(src=app.get_asset_url('FOOD_WASTE.png'), alt="Food Image",style={'width': '100%'}),
+        
+        # Dashboard Description
+        html.P('The Food Loss Quantity Dashboard provides an interactive visualization of food loss quantities across various dimensions, including country, commodity, food group, and year.',
+            style={'text-align':'justify','padding':'15px', 'font-size':'26px'}),
+        html.P('It offers insights into the scale and distribution of food loss, aiding stakeholders in understanding and addressing food wastage on a global scale. Users can explore trends, patterns, and disparities in food loss, enabling informed decision-making and targeted interventions.',
+            style={'text-align':'justify','padding':'15px', 'font-size':'26px'}),
+                    
+                ], style={'color': 'white', 'margin': '30px'})
     
-    # Sidebar Title
-    html.H1('Food Loss',className='text font-weight-bold font-weight-bolder text-center display-2'),
-    html.Hr(),
-    
-    # Sidebar Image
-    html.Img(src=app.get_asset_url('FOOD_WASTE.png'), alt="Food Image",style={'width': '100%'}),
-    
-    # Dashboard Description
-    html.P('The Food Loss Quantity Dashboard provides an interactive visualization of food loss quantities across various dimensions, including country, commodity, food group, and year.',
-        style={'text-align':'justify','padding':'15px', 'font-size':'26px'}),
-    html.P('It offers insights into the scale and distribution of food loss, aiding stakeholders in understanding and addressing food wastage on a global scale. Users can explore trends, patterns, and disparities in food loss, enabling informed decision-making and targeted interventions.',
-        style={'text-align':'justify','padding':'15px', 'font-size':'26px'}),
-                
-            ], style={'color': 'white', 'margin': '30px'})
+    return layout
 
 
 
